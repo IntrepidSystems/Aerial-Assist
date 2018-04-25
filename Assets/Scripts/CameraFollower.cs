0b1c78cce6f53cd0f7ef39791064bdfec004ad6c
Mitchel Stokes <37234862+MitchStokes@users.noreply.github.com>
2018-04-24 19:30:19 -0700

    Added basic shooting with space bar
    Added starter shooting implementation with space bar.

---
+++ Assets/Scripts/CameraFollower.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour {
+
+    public GameObject follow;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        transform.LookAt(follow.transform);
+	}
+
+}
--- Assets/Scripts/TestControlScript.cs
+++ Assets/Scripts/TestControlScript.cs
@@ -5,14 +5,24 @@ using UnityEngine;
 public class TestControlScript : MonoBehaviour {
 
     private Rigidbody body;
+    public GameObject prefab;
 
 	void Start () {
         body = GetComponent<Rigidbody>();
 	}
-	
+
+    bool wasSpacedLast = false;
+
 	void Update () {
         body.AddForce(body.transform.forward * Time.deltaTime * 1000.0f * Input.GetAxis("Vertical"), ForceMode.VelocityChange);
         body.AddTorque(new Vector3(0.0f, Time.deltaTime * 100000000.0f * Input.GetAxis("Horizontal"), 0.0f));
+
+        if(!wasSpacedLast && Input.GetKey("space")) {
+            GameObject ball = Instantiate(prefab);
+            ball.transform.position = body.transform.position + new Vector3(0.0f, 24.0f, 0.0f);
+            ball.GetComponent<Rigidbody>().AddForce((800.0f * (body.transform.forward + new Vector3(0.0f, 0.5f, 0.0f))) + body.velocity, ForceMode.VelocityChange);
+        }
+        wasSpacedLast = Input.GetKey("space");
 	}
 
 }
