e4b76a8cddcd4a786dbec548cb716edfdde51e07
Mitchel Stokes <37234862+MitchStokes@users.noreply.github.com>
2018-04-24 21:45:44 -0700

    Added high/low goal sensors
    Added sensors in the goals that when the ball passes through them, despawn the ball.

---
+++ Assets/Scripts/DespawnBall.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DespawnBall : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.tag == "Blue High Goal Sensor" || other.tag == "Red High Goal Sensor" || other.tag == "Blue Low Goal Sensor" || other.tag == "Red Low Goal Sensor") {
+            Invoke("DestroyBall", 1.0f);
+        }
+    }
+
+    public void DestroyBall() {
+        Destroy(gameObject);
+    }
+
+}
