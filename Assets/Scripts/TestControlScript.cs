b6a5de2e0300e93224e7e4bcfe17d5a64b848536
Mitchel Stokes <37234862+MitchStokes@users.noreply.github.com>
2018-04-24 18:43:13 -0700

    Created full field. added test balls and robot cube
    Created the entire field minus some minor details, added colliders and physics stuff where necessary, and added a test robot cube to drive around and push balls in the low goal.

---
+++ Assets/Scripts/TestControlScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestControlScript : MonoBehaviour {
+
+    private Rigidbody body;
+
+	void Start () {
+        body = GetComponent<Rigidbody>();
+	}
+	
+	void Update () {
+        body.AddForce(body.transform.forward * Time.deltaTime * 1000.0f * Input.GetAxis("Vertical"), ForceMode.VelocityChange);
+        body.AddTorque(new Vector3(0.0f, Time.deltaTime * 100000000.0f * Input.GetAxis("Horizontal"), 0.0f));
+	}
+
+}
